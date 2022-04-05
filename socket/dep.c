cab677d590124affc2e3ca66641da814f97bb452
Joaquin <joaquinvergara070@gmail.com>
2022-04-05 15:54:28 -0300

    added DB

--- socket/dep.c
+++ socket/dep.c
@@ -1,21 +1,15 @@
+// Dependencies file
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 #include "dep.h"
 
-int foo (int c) {
-
-return ++c;
-
-}
-
-
 char* parse_command (char comm[], int s) {
 	if (! strcmp(comm,"mensaje_de_saludo")) {
 		return SUCCESS_CODE " FTP Server v.1.0";
 	}
-	else if (! strcmp(comm,"QUIT")) {
+	else if (! strcmp(comm,"quit")) {
 		//aca falta cerrar el socket
 		return GB_CODE " Goodbye";
 	}
--- socket/server.c
+++ socket/server.c
@@ -8,76 +8,76 @@
 
 int main (int argc, char* argv[]) {
 
-if (argc != 2) {
-	fprintf(stderr,"No se recibieron la cantidad correcta de argumentos\nUso: ./server <puerto de donde se escuchara al cliente>\n");
-	exit(1);
-}
+    if (argc != 2) {
+        fprintf(stderr,"No se recibieron la cantidad correcta de argumentos\nUso: ./server <puerto de donde se escuchara al cliente>\n");
+        exit(1);
+    }
 
-int status, sockfd, sockstoragefd, bnd;
-struct addrinfo h;
-struct addrinfo *svinfo;
-struct sockaddr_storage their_addr;
-socklen_t addr_size;
-
-
-memset(&h, 0, sizeof(h)); // se asegura de igualar a 0 cada caracter de h
-
-h.ai_family = AF_INET; //IPV4
-h.ai_socktype = SOCK_STREAM; //TCP
-h.ai_flags = AI_PASSIVE; // usar la ip del host actual
-
-status = getaddrinfo(NULL, argv[1], &h, &svinfo);
-
-// svinfo es una lista enlazada que contiene estructuras del tipo addrinfo
-
-if (status != 0) {
-	fprintf(stderr, "error en la conexion: %s\n", gai_strerror(status));
-	return -1;
-}
-
-sockfd = socket(svinfo->ai_family, svinfo->ai_socktype, svinfo->ai_protocol);
-
-// bindear el socket recien creado con la ip al puerto pasado al getaddrinfo
-bnd = bind(sockfd, svinfo->ai_addr, svinfo->ai_addrlen);
-
-if (bnd < 0) {
-	fprintf(stderr,"Error durante el bindeo del socket: %s\n", gai_strerror(bnd));
-	return -1;
-}
-
-// escuchar conexiones que lleguen
-listen(sockfd, BACKLOG);
-
-printf("Puerto %s abierto escuchando conexiones...\n", argv[1]);
-
-// aceptar conexion que llegue, a la cual se le asignara un nuevo socket especifico que se utilizara para enviar/recibir flujo
-
-addr_size = sizeof their_addr;
-sockstoragefd = accept(sockfd, (struct sockaddr*)&their_addr, &addr_size);
-
-while(1) {
-
-// recibir 
-int received_bytes;
-char buff[MAX_BUFF_LENGTH];
-memset(buff,0,sizeof(buff)); //limpiar buffer por cada nuevo mensaje del host
-
-received_bytes = recv(sockstoragefd, buff, MAX_BUFF_LENGTH, 0);
+    int status, sockfd, sockstoragefd, bnd;
+    struct addrinfo h;
+    struct addrinfo *svinfo;
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
 
 
-if (received_bytes < 0) {
-	fprintf(stderr,"error: %s\n", gai_strerror(received_bytes));
-	return -1;
-}
-else {
-	char* response = parse_command(buff, sockstoragefd);
-	printf("host: %s\n", buff);
-	printf("%s\n",response);
-	//printf("bytes recibidos: %i\n", received_bytes);
-}
+    memset(&h, 0, sizeof(h)); // se asegura de igualar a 0 cada caracter de h
 
-}
-freeaddrinfo(svinfo); //importante liberar la memoria una vez terminada la lista enlazada
-return 0;
+    h.ai_family = AF_INET; //IPV4
+    h.ai_socktype = SOCK_STREAM; //TCP
+    h.ai_flags = AI_PASSIVE; // usar la ip del host actual
+
+    status = getaddrinfo(NULL, argv[1], &h, &svinfo);
+
+    // svinfo es una lista enlazada que contiene estructuras del tipo addrinfo
+
+    if (status != 0) {
+        fprintf(stderr, "error en la conexion: %s\n", gai_strerror(status));
+        return -1;
+    }
+
+    sockfd = socket(svinfo->ai_family, svinfo->ai_socktype, svinfo->ai_protocol);
+
+    // bindear el socket recien creado con la ip al puerto pasado al getaddrinfo
+    bnd = bind(sockfd, svinfo->ai_addr, svinfo->ai_addrlen);
+
+    if (bnd < 0) {
+        fprintf(stderr,"Error durante el bindeo del socket: %s\n", gai_strerror(bnd));
+        return -1;
+    }
+
+    // escuchar conexiones que lleguen
+    listen(sockfd, BACKLOG);
+
+    printf("Puerto %s abierto escuchando conexiones...\n", argv[1]);
+
+    // aceptar conexion que llegue, a la cual se le asignara un nuevo socket especifico que se utilizara para enviar/recibir flujo
+
+    addr_size = sizeof their_addr;
+    sockstoragefd = accept(sockfd, (struct sockaddr*)&their_addr, &addr_size);
+
+    while(1) {
+
+        // recibir 
+        int received_bytes;
+        char buff[MAX_BUFF_LENGTH];
+        memset(buff,0,sizeof(buff)); //limpiar buffer por cada nuevo mensaje del host
+
+        received_bytes = recv(sockstoragefd, buff, MAX_BUFF_LENGTH, 0);
+
+
+            if (received_bytes < 0) {
+                fprintf(stderr,"error: %s\n", gai_strerror(received_bytes));
+                return -1;
+            }
+            else {
+                char* response = parse_command(buff, sockstoragefd);
+                printf("host: %s\n", buff);
+                printf("%s\n",response);
+                //printf("bytes recibidos: %i\n", received_bytes);
+            }
+
+    }
+    freeaddrinfo(svinfo); //importante liberar la memoria una vez terminada la lista enlazada
+    return 0;
 
 }
